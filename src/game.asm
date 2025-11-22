2da8a46d2409a1155a3bbad750364097af0e8232
Anthony Bongers <bongers.anthony@gmail.com>
2025-11-21 20:32:17 -0500

    Prevent garbase SRAM on first launch

--- src/game.asm
+++ src/game.asm
@@ -10,10 +10,63 @@
 .include "screens.asm"
 .include "audio.asm"
 
+.segment "SAV"
+MagicValue1: .res 1
+MagicValue2: .res 1
+
 .segment "ZEROPAGE"
 IsDrawComplete: .res 1                 ; Flag to indicate when VBlank is done drawing.
+NeedsSRAMUpdate: .res 1
 
 .segment "CODE"
+
+;; Set the SRAM values on first launch of the game.
+;; Check for a two byte magic value.
+;; If that value isn't set, reset all SRAM values.
+;; Then set the magic value for next runs.
+;;
+;; This is done because SRAM may be in a random state on actual carts.
+;; Emulators are usually all zeros in SRAM, so bad data was never seen.
+.proc FirstLaunchClearSRAM
+  ;; Set to needing to clear by default.
+  lda #1
+  sta NeedsSRAMUpdate
+
+  ;; If the first magic byte doesn't match, jump to clearing SRAM.
+  lda MagicValue1
+  cmp #92
+  bne EndMagicValueCheck
+
+  ;; If the second magic byte doesn't match, jump to clearing SRAM.
+  lda MagicValue2
+  cmp #112
+  bne EndMagicValueCheck
+
+  ;; If we get here, both magic bytes matched, so don't clear the SRAM.
+  lda #0
+  sta NeedsSRAMUpdate
+
+  EndMagicValueCheck:
+
+  ;; If we need to clear SRAM...
+  lda NeedsSRAMUpdate
+  cmp #0
+  beq :+
+    ;; Zero out all game save data.
+    lda #0
+    sta PlayerProgressWorld
+    sta PlayerProgressLevel
+    sta PlayerProgressTutorialCompleted
+
+    ;; Then set the magic bytes.
+    lda #92
+    sta MagicValue1
+    lda #112
+    sta MagicValue2
+  :
+
+  rts
+.endproc
 
 ;; Lock execution until the NMI is done drawing.
 .proc WaitForVBlank
@@ -27,6 +80,8 @@ IsDrawComplete: .res 1                 ; Flag to indicate when VBlank is done dr
 ;; Reset ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 Reset:
   INIT_NES
+
+  jsr FirstLaunchClearSRAM
 
   jsr BgCopyReset
 
@@ -50,7 +105,7 @@ Reset:
 NMI:
   PUSH_REGS                            ; Save registers from before the interrupt.
 
-  ;; Show sprites and background, don't crop the left of the screen. 
+  ;; Show sprites and background, don't crop the left of the screen.
   lda #(PPU_MASK_FLAGS::SHOW_LEFT_BACKGROUND | PPU_MASK_FLAGS::SHOW_LEFT_SPRITES | PPU_MASK_FLAGS::SHOW_BACKGROUND | PPU_MASK_FLAGS::SHOW_SPRITES)
   sta PPU_MASK
 
@@ -80,4 +135,3 @@ IRQ:
 .word NMI
 .word Reset
 .word IRQ
-
